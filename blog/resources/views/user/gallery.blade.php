88210315c5e308fbaac3cc9ee3e0fb01955cf9bc
Piyaphan <piyaphan6929@gmail.com>
2017-05-11 00:12:48 +0700

    Add Gallery

--- blog/resources/views/user/gallery.blade.php
+++ blog/resources/views/user/gallery.blade.php
@@ -8,7 +8,8 @@
 @endsection
 
 @section('content')
-<div class="section secondary-section" id="about">
+    <div class="section secondary-section" id="about">
+        <div class="triangle"></div>
         <div class="container">
             <div class="title">
                 <h1>Gallery</h1>
@@ -16,68 +17,87 @@
             <div class="row-fluid team">
                 <div class="span4" id="first-person">
                     <div class="thumbnail">
-                        <img src="images/Portfolio01.png" alt="team 1">
-                        <h3>John Doe</h3>
+                        <img src="images/gallery/Sribluegame.jpg" alt="team 1">
+                        <h3>สีบลูเกมครั้งที่ 17</h3>
+                        <ul class="social">
+                            <li>
+                                <a href="">
+                                    <span class="icon-facebook-circled"></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <span class="icon-twitter-circled"></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <span class="icon-linkedin-circled"></span>
+                                </a>
+                            </li>
+                        </ul>
                         <div class="mask">
-                            <h2>Copywriter</h2>
-                            <p>When you stop expecting people to be perfect, you can like them for who they are.</p>
+                            <h2>สีบลูเกมครั้งที่ 17</h2>
+                            <p>การแข่งขันกี่ฬามหาวิทยาลัย สีบลูเกมครั้งที่17</p>
                         </div>
                     </div>
                 </div>
                 <div class="span4" id="second-person">
                     <div class="thumbnail">
-                        <img src="images/Portfolio01.png" alt="team 1">
-                        <h3>John Doe</h3>
+                        <img src="images/gallery/krue.jpg" alt="team 1">
+                        <h3>ไหว์ครู 2016</h3>
+                        <ul class="social">
+                            <li>
+                                <a href="">
+                                    <span class="icon-facebook-circled"></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <span class="icon-twitter-circled"></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <span class="icon-linkedin-circled"></span>
+                                </a>
+                            </li>
+                        </ul>
                         <div class="mask">
-                            <h2>Designer</h2>
-                            <p>When you stop expecting people to be perfect, you can like them for who they are.</p>
+                            <h2>ไหว์ครู 2016</h2>
+                            <p>มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตภูเก็ตจัดพิธีไหว้ึครู เนื่องในวันครู</p>
                         </div>
                     </div>
                 </div>
                 <div class="span4" id="third-person">
                     <div class="thumbnail">
-                        <img src="images/Portfolio01.png" alt="team 1">
-                        <h3>John Doe</h3>
+                        <img src="images/gallery/5wit.jpg" alt="team 1">
+                        <h3>การแข่งขันกีฬา5วิทยาเขต</h3>
+                        <ul class="social">
+                            <li>
+                                <a href="">
+                                    <span class="icon-facebook-circled"></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <span class="icon-twitter-circled"></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <span class="icon-linkedin-circled"></span>
+                                </a>
+                            </li>
+                        </ul>
                         <div class="mask">
-                            <h2>Photographer</h2>
-                            <p>When you stop expecting people to be perfect, you can like them for who they are.</p>
-                        </div>
-                    </div>
-                </div>
-            <
-
-            <div class="row-fluid team">
-                <div class="span4" id="first-person">
-                    <div class="thumbnail">
-                        <img src="images/Portfolio01.png" alt="team 1">
-                        <h3>John Doe</h3>
-                        <div class="mask">
-                            <h2>Copywriter</h2>
-                            <p>When you stop expecting people to be perfect, you can like them for who they are.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="span4" id="second-person">
-                    <div class="thumbnail">
-                        <img src="images/Portfolio01.png" alt="team 1">
-                        <h3>John Doe</h3>
-                        <div class="mask">
-                            <h2>Designer</h2>
-                            <p>When you stop expecting people to be perfect, you can like them for who they are.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="span4" id="third-person">
-                    <div class="thumbnail">
-                        <img src="images/Portfolio01.png" alt="team 1">
-                        <h3>John Doe</h3>
-                        <div class="mask">
-                            <h2>Photographer</h2>
-                            <p>When you stop expecting people to be perfect, you can like them for who they are.</p>
+                            <h2>การแข่งขันกีฬา5วิทยาเขต</h2>
+                            <p>มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตภูเก็ตร่วมการแข่งขันกีฬา5วิทยาเขต ณ ม.อ. สุราษฏร์</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <!-- Gallery section end -->
 @endsection
