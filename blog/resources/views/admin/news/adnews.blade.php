6758dcc12e103d2261cb2123f4bf571ff48cac20
AKKHARAPHON  SOMKAEO <sakamoto.a2014@gmail.com>
2017-05-10 21:56:52 +0700

    add News Management

--- blog/resources/views/admin/news/adnews.blade.php
+++ blog/resources/views/admin/news/adnews.blade.php
@@ -8,5 +8,50 @@
 @endsection
 
 @section('content')
-
+<div class="row">
+	    <div class="col-lg-12 margin-tb">
+	        <div class="pull-left">
+	            <h2>News Managemant</h2>
+	        </div>
+	        <div class="pull-right">
+	            <a class="btn btn-success" href=""> Create New User</a>
+	        </div>
+	    </div>
+	</div><br>
+<table class="table table-striped table-hover">
+		<tr>
+			<th>No</th>
+			<th>Title</th>
+			<th>Deteil</th>
+			<th>Post By</th>
+			<th>Email</th>
+			
+			<th width="280px">Action</th>
+		</tr>
+	
+	<tr>
+		<td>1</td>
+		<td>PSU Walk Rally Episode 1</td>
+		<td>เชิญนักศึกษาร่วมโครงการร่วมแข่งขัน PSU Walk Rally Episode 1 : Penang Survival</td>
+		<td>Aekkamon.N</td>
+		<td>aekkamon@gmail.com</td>
+		<td>
+			<a class="btn btn-info" href="">Appove</a>
+			<a class="btn btn-primary" href="">Edit</a>
+			<a class="btn btn-success" href="">Delete</a>
+		</td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td>ขอเชิญร่วมประกวดภาพถ่าย</td>
+		<td>ขอเชิญร่วมประกวดภาพถ่าย ในหัวข้อ พระราชบิดาในมุมมองของฉัน</td>
+		<td>Aekkamon.N</td>
+		<td>aekkamon@gmail.com</td>
+		<td>
+			<a class="btn btn-info" href="">Appove</a>
+			<a class="btn btn-primary" href="">Edit</a>
+			<a class="btn btn-success" href="">Delete</a>
+		</td>
+	</tr>
+	</table>
 @endsection
