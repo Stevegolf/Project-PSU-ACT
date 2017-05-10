bb3a76530bb99bd386be85beaad1cb564e67e473
Piyaphan <piyaphan6929@gmail.com>
2017-05-11 03:36:34 +0700

    delete photo

--- blog/resources/views/layouts/theme.blade.php
+++ blog/resources/views/layouts/theme.blade.php
@@ -67,7 +67,7 @@
                         </li>
                         <li><a href="/document">Document</a></li>
                         <li><a href="/contact">Contact</a></li>
-                        <li><a href="/loginpsu">Login</a></li>
+                        <li><a href="">Login</a></li>
                     </ul>
                 </div>
 
