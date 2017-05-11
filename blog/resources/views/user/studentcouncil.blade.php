92507323ef2d1bb8acda6b563cec02a5baddf1fc
Pongsakon Chamnankit <s5730211008@phuket.psu.ac.th>
2017-05-12 02:31:28 +0700

    update for organization chart for student_union and student_council

--- blog/resources/views/user/studentcouncil.blade.php
+++ blog/resources/views/user/studentcouncil.blade.php
@@ -7,6 +7,44 @@
 
 @endsection
 
+
 @section('content')
+
+<style>
+  text {
+      color: #478bca;
+      text-align: center;
+  }
+  tabs{
+    tab-size:4;
+  }
+</style>
+
+<div class="triangle"></div>
+  <div class="section secondary-section" id="document">
+      <div class="container">
+      <div class="title">
+
+          <h1>Student Council</h1>
+
+      </div>
+      <text><h4><tabs>สภานักศึกษา คือ องค์กรหนึ่งของนักศึกษา ที่ถูกแต่งตั้งขึ้นโดยมหาวิทยาลัยเพื่อที่จะดูแลการดำเนินกิจกรรมต่างๆของนักศึกษาให้เป็นไปตามระเบียบของมหาวิทยาลัย…..องค์กรนักศึกษาเพื่อการศึกษานั่นเอง บทบาทหน้าที่ ควบคุมดูแลการทำงานขององค์การบริหารองค์การนักศึกษา ชมรมต่างๆรวมถึงสโมสรนักศึกษาคณะต่างๆ ในมหาวิทยาลัย พิทักษ์สิทธิ์ต่างๆของนักศึกษา พิจารณาจัดสรรงบประมาณ ในการจัดกิจกรรมต่างๆของนักศึกษา</h4></text><br><br>
+      <text><h2>Student Council Chart</h2></text>
+      <img src="images/pic_studentunion/studentcouncil_chart.png">
+  </div>
+</div>
+
+<text><div class="container">
+    <div class="span9 center contact-info">
+
+        <h4>องค์การบริหาร องค์การนักศึกษา มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตภูเก็ต </h4>
+        <h4>80 หมู่ 1 ถ.วิชิตสงคราม อ.กะทู้ จ.ภูเก็ต 83120</h4>
+        <h4>Phone: 0-7627-1234</h4>
+        <h4>Student_union@gmail.com</h4>
+        <div class="title">
+            <h3>We Are Social</h3>
+        </div>
+    </div></text>
+
 
 @endsection
--- blog/resources/views/user/studentunion.blade.php
+++ blog/resources/views/user/studentunion.blade.php
@@ -8,19 +8,43 @@
 @endsection
 
 @section('content')
+
+<style>
+  text {
+      color: #478bca;
+      text-align: center;
+  }
+  tabs{
+    tab-size:4;
+  }
+</style>
+
 <div class="triangle"></div>
   <div class="section secondary-section" id="document">
       <div class="container">
       <div class="title">
+
           <h1>Student Union</h1>
 
       </div>
-      <img src="images/bg_studentunion/bg.png">
+      <text><h4><tabs>มหาวิทยาลัยสงขลานครินทร์มีนโยบายที่จะสนับสนุนให้นักศึกษาได้มีโอกาสเข้าร่วมกิจกรรมตามความถนัดและความสนใจ นอกเหนือจากการฟังบรรยายในห้องเรียน</tab4>
+        ตลอดจนการฝึกฝนทักษะเพื่อที่จะให้เป็นบัณฑิตที่เพียบพร้อมไปด้วยความรู้ความสามารถ บุคลิกภาพที่ดี มีวิจารณญาณ เสียสละต่อส่วนรวม มีคุณธรรม จริยธรรม และสุขภาพพลานามัยที่แข็งแรง
+        เป็นบัณฑิตที่มีคุณภาพและคุณธรรมสมบูรณ์ทั้งทางด้านสติปัญญา ร่างกายและจิตใจ โดยนักศึกษาสามารถเข้าร่วมกิจกรรมต่างๆ ตามความสนใจ</h4></text><br><br>
+      <text><h2>Student Union Chart</h2></text>
+      <img src="images/pic_studentunion/studentunion_chart.png">
   </div>
 </div>
 
+<text><div class="container">
+    <div class="span9 center contact-info">
 
-
-
+        <h4>องค์การบริหาร องค์การนักศึกษา มหาวิทยาลัยสงขลานครินทร์ วิทยาเขตภูเก็ต </h4>
+        <h4>80 หมู่ 1 ถ.วิชิตสงคราม อ.กะทู้ จ.ภูเก็ต 83120</h4>
+        <h4>Phone: 0-7627-1234</h4>
+        <h4>Student_union@gmail.com</h4>
+        <div class="title">
+            <h3>We Are Social</h3>
+        </div>
+    </div></text>
 
 @endsection
