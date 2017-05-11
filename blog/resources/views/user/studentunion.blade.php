c2aecb0a92d65da94f012e76260d9c0a261393da
Pongsakon Chamnankit <s5730211008@phuket.psu.ac.th>
2017-05-12 01:11:15 +0700

    add picture for studentunion

--- blog/resources/views/user/studentunion.blade.php
+++ blog/resources/views/user/studentunion.blade.php
@@ -8,6 +8,19 @@
 @endsection
 
 @section('content')
+<div class="triangle"></div>
+  <div class="section secondary-section" id="document">
+      <div class="container">
+      <div class="title">
+          <h1>Student Union</h1>
+
+      </div>
+      <img src="images/bg_studentunion/bg.png">
+  </div>
+</div>
+
+
+
 
 
 @endsection
