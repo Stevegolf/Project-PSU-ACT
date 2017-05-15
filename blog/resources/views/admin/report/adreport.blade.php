9db7bda06048fc6a839876507739f77564c94e96
Piyaphan <piyaphan6929@gmail.com>
2017-05-15 10:01:13 +0700

    Update report

--- blog/resources/views/admin/report/adreport.blade.php
+++ blog/resources/views/admin/report/adreport.blade.php
@@ -56,29 +56,7 @@
                 </a>
             </div>
         </div>
-        <div class="col-lg-3 col-md-3">
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <i class="glyphicon glyphicon-share-alt fa-3x"></i>
-                        </div>
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">9</div>
-                            <div>รายงานประเมินผลโครงการหลังการจัดกิจกรรม</div>
-                        </div>
-                    </div>
-                </div>
-                <a href="">
-                    <div class="panel-footer">
-                        <span class="pull-left">View</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
+        
         <div class="col-lg-3 col-md-3">
             <div class="panel panel-info">
                 <div class="panel-heading">
